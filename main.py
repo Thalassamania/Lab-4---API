fde34d42795b396fd4ddf76dbd85ca0701bf1e5c
thalassamania <j.charryg@uniandes.edu.co>
2022-11-02 20:42:27 -0700

    Inicio

---
+++ main.py
@@ -0,0 +1,26 @@
+from joblib import load
+from typing import Optional
+from fastapi import FastAPI
+from DataModel import DataModel
+import pandas as pd
+
+
+app = FastAPI()
+
+@app.post("/predict")
+def make_predictions(dataModel: DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    model = load("assets/modelo.joblib")
+    result = model.predict(df)
+    return result
+
+
+@app.get("/")
+def read_root():
+   return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+   return {"item_id": item_id, "q": q}
